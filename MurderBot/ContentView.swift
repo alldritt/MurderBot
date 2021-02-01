d4ce1a419e4522e45a9b12d8b6c913e4d262e8b5
Mark Alldritt <alldritt@latenightsw.com>
2021-02-01 12:13:11 -0800

    Reorganize SwiftUI code for better readability.

--- MurderBot/ContentView.swift
+++ MurderBot/ContentView.swift
@@ -10,9 +10,6 @@ import CardStack
 import ActivityIndicatorView
 
 
-let cardColors = [#colorLiteral(red: 0.7265671492, green: 0.7860677242, blue: 0.7937102318, alpha: 1), #colorLiteral(red: 0.8190054893, green: 0.8204569221, blue: 0.7675603628, alpha: 1), #colorLiteral(red: 0.7900114655, green: 0.8009047508, blue: 0.7654793262, alpha: 1), #colorLiteral(red: 0.9045919776, green: 0.9105549455, blue: 0.8752011657, alpha: 1), #colorLiteral(red: 0.5154729486, green: 0.5264404416, blue: 0.486608386, alpha: 1), #colorLiteral(red: 0.7469534278, green: 0.8222278953, blue: 0.8033685088, alpha: 1), #colorLiteral(red: 0.9165374637, green: 0.9220481515, blue: 0.9042716622, alpha: 1), #colorLiteral(red: 0.8401653171, green: 0.855901897, blue: 0.8247912526, alpha: 1), #colorLiteral(red: 0.8039962649, green: 0.8246852756, blue: 0.7934988141, alpha: 1), #colorLiteral(red: 0.8850758672, green: 0.8908110261, blue: 0.8642533422, alpha: 1)]
-
-
 struct CardOverlay: View {
     //  CardOverlay displays the 'title', in this case the Plot seed value, of the card.
     
@@ -21,7 +18,7 @@ struct CardOverlay: View {
     var body: some View {
         ZStack {
             Text(text)
-                .font(.footnote)
+                .font(.body)
                 .padding(EdgeInsets(top: 5, leading: 7, bottom: 5, trailing: 9))
                 .foregroundColor(.black)
         }
@@ -42,9 +39,12 @@ struct CardView: View {
     @ObservedObject var plotFetch: Plot.Fetch
     @State var showActivity = true
 
+    let cardColors = [#colorLiteral(red: 0.7265671492, green: 0.7860677242, blue: 0.7937102318, alpha: 1), #colorLiteral(red: 0.8190054893, green: 0.8204569221, blue: 0.7675603628, alpha: 1), #colorLiteral(red: 0.7900114655, green: 0.8009047508, blue: 0.7654793262, alpha: 1), #colorLiteral(red: 0.9045919776, green: 0.9105549455, blue: 0.8752011657, alpha: 1), #colorLiteral(red: 0.5154729486, green: 0.5264404416, blue: 0.486608386, alpha: 1), #colorLiteral(red: 0.7469534278, green: 0.8222278953, blue: 0.8033685088, alpha: 1), #colorLiteral(red: 0.9165374637, green: 0.9220481515, blue: 0.9042716622, alpha: 1), #colorLiteral(red: 0.8401653171, green: 0.855901897, blue: 0.8247912526, alpha: 1), #colorLiteral(red: 0.8039962649, green: 0.8246852756, blue: 0.7934988141, alpha: 1), #colorLiteral(red: 0.8850758672, green: 0.8908110261, blue: 0.8642533422, alpha: 1)]
     let cardHeightScale = CGFloat(1.65)
+    let cardCornerRadius = CGFloat(12)
+    let cardShadowRadius = CGFloat(4)
 
-    func calcCardSize(_ size: CGSize) -> CGSize {
+    private func calcCardSize(_ size: CGSize) -> CGSize {
         let h = floor(size.width * cardHeightScale)
         
         if h > size.height {
@@ -79,8 +79,8 @@ struct CardView: View {
                     
                 }
                 .background(Color(cardColor))
-                .cornerRadius(12)
-                .shadow(radius: 4)
+                .cornerRadius(cardCornerRadius)
+                .shadow(radius: cardShadowRadius)
                 .overlay(CardOverlay(text: String(plot.seed)),
                          alignment: .bottomTrailing)
                 .gesture(LongPressGesture(minimumDuration: 1) // Long Press views plot on https://midsomerplots.net
@@ -107,8 +107,8 @@ struct CardView: View {
                            alignment: .bottom)
                 }
                 .background(Color(cardColor))
-                .cornerRadius(12)
-                .shadow(radius: 4)
+                .cornerRadius(cardCornerRadius)
+                .shadow(radius: cardShadowRadius)
             }
         }
     }
@@ -117,14 +117,13 @@ struct CardView: View {
 
 struct ContentView: View {
     @Environment(\.openURL) var openURL
-    @State var fetches = [
-        Plot.Fetch(),
-        Plot.Fetch(),
-        Plot.Fetch(),
-        Plot.Fetch(),
-        Plot.Fetch(),
-        Plot.Fetch(),
-        Plot.Fetch()]
+    @State var fetches = [Plot.Fetch(),  // seed the CardStack with some plots...
+                          Plot.Fetch(),
+                          Plot.Fetch(),
+                          Plot.Fetch(),
+                          Plot.Fetch(),
+                          Plot.Fetch(),
+                          Plot.Fetch()]
 
     var body: some View {
         VStack {
